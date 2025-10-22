71bb02b7c728334b6dccc7a9b021e6caac84c7a1
UTAmegg <160442205+UTAmegg@users.noreply.github.com>
2025-10-22 18:00:07 -0500

    Add files via upload

--- core/extractor.py
+++ core/extractor.py
@@ -70,33 +70,33 @@ class BottleneckBlock(nn.Module):
             self.norm1 = nn.GroupNorm(num_groups=num_groups, num_channels=planes // 4)
             self.norm2 = nn.GroupNorm(num_groups=num_groups, num_channels=planes // 4)
             self.norm3 = nn.GroupNorm(num_groups=num_groups, num_channels=planes)
-            if not stride == 1:
+            if not (stride == 1 and in_planes == planes):
                 self.norm4 = nn.GroupNorm(num_groups=num_groups, num_channels=planes)
 
         elif norm_fn == 'batch':
             self.norm1 = nn.BatchNorm2d(planes // 4)
             self.norm2 = nn.BatchNorm2d(planes // 4)
             self.norm3 = nn.BatchNorm2d(planes)
-            if not stride == 1:
+            if not (stride == 1 and in_planes == planes):
                 self.norm4 = nn.BatchNorm2d(planes)
 
         elif norm_fn == 'instance':
             self.norm1 = nn.InstanceNorm2d(planes // 4)
             self.norm2 = nn.InstanceNorm2d(planes // 4)
             self.norm3 = nn.InstanceNorm2d(planes)
-            if not stride == 1:
+            if not (stride == 1 and in_planes == planes):
                 self.norm4 = nn.InstanceNorm2d(planes)
 
         elif norm_fn == 'none':
             self.norm1 = nn.Sequential()
             self.norm2 = nn.Sequential()
             self.norm3 = nn.Sequential()
-            if not stride == 1:
+            if not (stride == 1 and in_planes == planes):
                 self.norm4 = nn.Sequential()
 
-        if stride == 1:
+        # Need projection if stride changes OR channels change
+        if stride == 1 and in_planes == planes:
             self.downsample = None
-
         else:
             self.downsample = nn.Sequential(
                 nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride), self.norm4)
